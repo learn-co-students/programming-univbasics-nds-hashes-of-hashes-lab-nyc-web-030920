f6c426153d6441176aa293c74f796d45338e182e
Robert Keller <robert_keller44@icloud.com>
2019-12-28 20:13:39 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,3 +1,4 @@
+
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
@@ -31,6 +32,29 @@
 # }
 
 def naming_system
+   sysem_hash = {label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+        sub_category: {
+          label: "Class",
+            sub_category: {
+              label: "Order",
+                sub_category: {
+                  label: "Family",
+                    sub_category: {
+                      label: "Genus",
+                        sub_category: {
+                          label: "Species",
+                          sub_category: nil
+                          
+                        }
+                    }
+                }
+                  
+    }
+  }
+}
+}
   # Remember:
   #  Kingdom
   #  Phylum
